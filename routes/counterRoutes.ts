ef9d41741a46a189239032fa0c9e91bc039867d9
omerpeled6 <opeled6@gmail.com>
2025-10-03 16:37:21 +0300

    make CODE CLEANER

--- routes/counterRoutes.ts
+++ routes/counterRoutes.ts
@@ -7,7 +7,6 @@ router.get("/", async (req, res) => {
   const counter =
     (await Counter.findOne()) || (await Counter.create({ count: 0 }));
   res.json({ counter });
-  console.log(counter.count);
 });
 
 router.post("/update", async (req, res) => {
@@ -18,7 +17,6 @@ router.post("/update", async (req, res) => {
     { new: true, upsert: true }
   );
   res.json({ counter });
-  console.log(counter.count);
 });
 
 export default router;
--- server.ts
+++ server.ts
@@ -1,4 +1,4 @@
-import express, { Request, Response } from "express";
+import express from "express";
 import mongoose from "mongoose";
 import counterRoutes from "./routes/counterRoutes.js";
 import cors from "cors";
@@ -10,29 +10,15 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.get("/", (req: Request, res: Response) => {
-  res.send("Hello World omer mogno db");
-});
-
-app.get("/test", (req: Request, res: Response) => {
-  res.json({ message: "Hello World3" });
-});
-
-// Register counter routes
+app.get("/", (req, res) => res.send("Counter API"));
 app.use("/counter", counterRoutes);
 
-// Connect to MongoDB
 mongoose
   .connect(MONGOURI, { dbName: "demo_db" })
   .then(() => {
     const PORT = process.env.PORT || 3000;
-    app.listen(PORT, () => {
-      console.log("Connected to db & listening on port", PORT);
-    });
+    app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
   })
-  .catch((error) => {
-    console.error("Database connection error:", error);
-  });
+  .catch(console.error);
 
-// Export for Vercel
 export default app;
