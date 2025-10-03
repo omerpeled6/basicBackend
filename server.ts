ec1c5e8417920248b5605bb7ec822dfc2691ace5
omerpeled6 <opeled6@gmail.com>
2025-10-03 14:02:25 +0300

    add

--- server.ts
+++ server.ts
@@ -27,16 +27,16 @@ app.listen(PORT, () => {
 // app.use("/counter", counterRoutes);
 
 // Connect to MongoDB
-// mongoose
-//   .connect(MONGOURI, { dbName: "demo_db" })
-//   .then(() => {
-//     app.listen(process.env.PORT || 3000, () => {
-//       console.log(
-//         "Connected to db & listening on port",
-//         process.env.PORT || 3000
-//       );
-//     });
-//   })
-//   .catch((error) => {
-//     console.error("Database connection error:", error);
-//   });
+mongoose
+  .connect(MONGOURI, { dbName: "demo_db" })
+  .then(() => {
+    app.listen(process.env.PORT || 3000, () => {
+      console.log(
+        "Connected to db & listening on port",
+        process.env.PORT || 3000
+      );
+    });
+  })
+  .catch((error) => {
+    console.error("Database connection error:", error);
+  });
